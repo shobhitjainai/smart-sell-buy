1e424448360fa0a94725ccf66adfeac8ea17579c
shobhitjainai <shobhit.j@arbutusinfotech.com>
2024-05-07 00:24:20 -0700

    worked on archive product icon

--- src/app/main/products/ProductCard.js
+++ src/app/main/products/ProductCard.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import StarIcon from '@mui/icons-material/Star';
 import { useDispatch } from 'react-redux';
 import { createArchieve } from 'app/store/userSlices/userSellingSlice';
 
-export default function ProductCard({ title, image, price, address,id, archive }) {
+export default function ProductCard({ title, image, price, address, id, archive }) {
   const [isFavorite, setIsFavorite] = React.useState(false);
   const dispatch = useDispatch();
 
@@ -15,25 +14,31 @@ export default function ProductCard({ title, image, price, address,id, archive }
     const data = {
       product_id: id,
     }
-    dispatch(createArchieve(data));
+    dispatch(createArchieve(data).then((res)=>{
+      console.log(res)
+    }));
   };
   return (
     <Card className='lg:w-full md:w-full p-10 h-[350px] rounded-sm relative'>
-       <CardMedia
+      <CardMedia
         component="img"
         alt="ecommerce"
         className="object-cover object-center w-full h-4/6 block "
         image={image.image}
       />
+      <div className='flex flex-end relative'>
+          <StarIcon
+            fontSize="large"
+            className={`ml-2 cursor-pointer absolute -top-20 right-2 ${!isFavorite ? (archive ? 'text-[#FFD700]' : 'text-gray-400') : (archive ? 'text-gray-400' : 'text-[#FFD700]')}`}
+            onClick={() => toggleFavorite(id)}
+          />
+        </div>
       <div className="flex items-center mb-4 ">
         <h3 className="text-gray-700 text-2xl tracking-widest title-font mb-1 pt-8 font-bold ">{title}</h3>
+
         
-        <FavoriteIcon
-          fontSize="large"
-          className={`ml-2 cursor-pointer absolute top-[40] right-5 ${ !isFavorite ? ( archive ? 'text-red-500' :  'text-gray-400'):( archive ? 'text-gray-400' :  'text-red-500')}`}
-          onClick={() => toggleFavorite(id)}
-        />
-    
+
+
       </div>
       <h2 className="text-gray-500 title-font text-lg font-medium">{address}</h2>
       <p className="mt-1 pt-4 text-blue-700 font-semibold">₹{price}</p>
