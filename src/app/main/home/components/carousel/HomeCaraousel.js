c225ae53f9c1c439ff55507833131941679bf2b2
purvisolankiai <purvi.s@arbutusinfotech.com>
2024-05-07 16:45:16 +0530

    product banner

--- src/app/main/home/components/carousel/HomeCaraousel.js
+++ src/app/main/home/components/carousel/HomeCaraousel.js
@@ -3,38 +3,10 @@ import Carousel from 'react-material-ui-carousel'
 import Items from './Items'
 import { useDispatch, useSelector } from 'react-redux';
 import { getBannerImages } from 'app/store/userSlices/userHomeSlice';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-
-const images = [
-    {
-        id: 1,
-      title: 'San Francisco – Oakland Bay Bridge, United States',
-      imgPath:
-        'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        id: 2,
-        title: 'Bird',
-      imgPath:
-        'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        id: 3,
-        title: 'Bali, Indonesia',
-        imgPath:
-        'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-    },
-    {
-        id: 4,
-        title: 'Goč, Serbia',
-        imgPath:
-        'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-  ];
 
 function HomeCaraousel(){
 
@@ -61,25 +33,20 @@ function HomeCaraousel(){
 const  {bannerImages} = useSelector((state) => state.userSlices.userHomeSlice);    
     return (
 
-        // <Carousel>
-        //     {
-        //         bannerImages?.map( (item) => 
-        //         <Items item={item.images[0]} />
-        //     )
-        //     }
-        // </Carousel>
-
         <>
         <Grid className='w-full'>
           <Grid>
             <Slider {...settings}>
           {
                 bannerImages?.map( (item) => (
-                  <div className='bg-white h-288  text-black'>
+                  <div className='bg-white h-288  text-black reltive'>
                     <img src={item.images[0].image} alt='caraousel-item' className='h-288 w-full object-center '/>
+                    <div className='absolute bottom-10 ml-4 '>
+                    <Typography>{item.price}</Typography>
+                    </div>
                   </div>
                 )
-                // <Items item={item.images[0]} />
+
             )
             }
             </Slider>
